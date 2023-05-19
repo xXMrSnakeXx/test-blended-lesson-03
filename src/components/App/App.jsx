5d2a92cc2571f723c0be989161f8fdb4dffd0c98
Vladyslav Rozhenko <pochta.vlad@ukr.net>
2023-05-19 22:55:54 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from 'components';
 import { CountrySearch, Home, Country } from 'pages';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { routes } from 'routes';
 
 export const App = () => {
@@ -12,6 +12,7 @@ export const App = () => {
           <Route path={routes.COUNTRY} element={<CountrySearch />} />
           <Route path={routes.COUNTRY_ID} element={<Country />} />
         </Route>
+        <Route path="*" element={<Navigate to={routes.HOME} replace />} />
       </Routes>
     </>
   );
